a180517c2aef99076a775776fee7770928c139c7
Jitendra Saini <stupidboysbusiness@gmail.com>
2024-12-18 01:36:14 +0530

    Authentication web-page

---
+++ Backend/Authentication/signup.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded" , function(){
+
+    const signupForm = document.getElementById('signupForm')
+    signupForm.addEventListener('submit' , function(e){
+        e.preventDefault()
+        const name = document.getElementById('name').value
+        const email = document.getElementById('email').value
+        const username = document.getElementById('username').value
+        const password = document.getElementById('password').value
+
+        const userData = { Name: name, Email: email,Username: username,Password: password}
+        registerUser(userData)
+    })
+})
+
+async function registerUser(userData){
+    try {
+        const response = await axios.post("Api link" , userData)    //   Api link need
+        if (response.data.success) {
+            window.location.href = 'not have link';               //   Api link need
+        } else {
+            alert('Invalid username or password. Please try again.');
+        }
+    }catch(error){
+        console.error('Error during API call:', error);
+            alert('Something went wrong. Please try again.')
+    }
+}
