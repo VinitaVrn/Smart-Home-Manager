acf70f5430a74b210cde0c7b05f8ede3a9d6afd8
Jitendra Saini <stupidboysbusiness@gmail.com>
2024-12-19 01:11:06 +0530

    Authentication web-page

---
+++ Frontend/SetupDevice/setupDevice.js
@@ -0,0 +1,45 @@
+document.getElementById("homePart").addEventListener("change", function () {
+    const customRoom = document.getElementById("customRoomDiv");
+
+    if (this.value === 'custom') {
+        customRoom.style.display = 'block';
+    } else {
+        customRoom.style.display = 'none';
+    }
+})
+
+async function addRoom(e){
+    e.preventDefault()
+    const roomName = document.getElementById("homePart").value
+    const customRoom = document.getElementById("customRoom").value
+    const device1 =  document.getElementById("device1").value
+    const device2 =  document.getElementById("device2").value
+    const device3 =  document.getElementById("device3").value
+    const device4 =  document.getElementById("device4").value
+    const device5 =  document.getElementById("device5").value
+    const device6 =  document.getElementById("device6").value
+
+   const newRoom = homePart === 'custom' ? customRoom :roomName ;
+   const devices = { Lamps: device1,TV: device2, Fan: device3, Light: device4,RGBLight: device5,SwitchBoard: device6 }
+
+   const roomData = {newRoom , devices}
+try{
+     let res = await axios.post(" Api need here" , roomData)   //Api Link Need here
+     console.log(res.data)
+     if(res.status == 200){
+        alert("Room Added Successfully")
+        document.getElementById("roomForm").reset()
+        document.getElementById("customRoomDiv").style.display = "none";
+     }else{
+        alert("Failed to add room. Please try again.")
+     }
+}catch(error){
+    console.error("Error adding room:", error);
+    alert("An error occurred while adding the room.");
+}
+}
+
+document.getElementById('roomForm').addEventListener('submit' , addRoom)
+function Dashboard() {
+    window.location.href = " dashboad link ";   // dashboard link need
+}
