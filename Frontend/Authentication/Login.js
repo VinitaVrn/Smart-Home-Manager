285362b3398b944608955588167e32823a53f348
PANKAJ SHARMA <sinerssins@gmail.com>
2024-12-19 17:37:03 +0530

    Login-Signup Details

---
+++ Frontend/Authentication/Login.js
@@ -0,0 +1,27 @@
+function handleLogin(e) {
+    e.preventDefault();
+    const username = document.getElementById('userName').value;
+    const password = document.getElementById('password').value;
+    const loginData = { Username: username, Password: password };
+    fetchData(loginData);
+}
+
+async function fetchData(loginData) {
+    console.log(loginData)
+    try {
+        const response = await axios.post(' link', loginData);
+        console.log(response.data)
+        if (response.data.success) {
+            window.location.href = 'dashborad link';  //link need
+         }  
+        }
+     catch (error) {
+        console.error('Error during login request:', error);
+        alert('Something went wrong. Please try again later.');
+    }
+}
+
+
+function toggleForms() {
+    window.location.href = 'Signup.html';
+}
